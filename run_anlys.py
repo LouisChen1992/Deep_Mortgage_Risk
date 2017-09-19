cf2561abf5706f8e1c31a9fa315f27e45472e247
Luyang Chen <LouisChen1992@hotmail.com>
2017-09-19 13:49:27 -0700

    debug

--- run_anlys.py
+++ run_anlys.py
@@ -7,5 +7,4 @@ path = '/vol/Numpy_data_subprime_Test_new'
 mode = 'anlys'
 dl = DataInRamInputLayer(path=path, mode=mode)
 ave_absolute_gradient = np.load('model/ave_absolute_gradient.npy')
-print covariate_ranking_by_ave_absolute_gradient(dl._idx2covariate, ave_absolute_gradient)
-
+print(covariate_ranking_by_ave_absolute_gradient(dl._idx2covariate, ave_absolute_gradient))
